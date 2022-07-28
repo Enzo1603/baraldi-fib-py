0c0da65b18d1193ac0a9d8d7b813936c5c3dfda6
Enzo1603 <96661578+Enzo1603@users.noreply.github.com>
2022-07-28 11:47:52 +0200

    add src directory

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import find_packages, setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="baraldi_fib_py",
     version="0.0.1",
     author="Enzo Baraldi",
     description="Calculates a Fibonacci number",
-    long_description="A basic library that calculates Fibonacci numbers",
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Enzo1603/baraldi-fib-py.git",
     install_requires=[],
---
+++ src/__init__.py
@@ -0,0 +1,2 @@
+def say_hello() -> None:
+    print("the Baraldi Fibonacci module is saying hello")
